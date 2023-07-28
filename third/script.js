da800093c224476b6e60b2c550f3352a6af55c42
XuanyuL <88788797+XuanyuL@users.noreply.github.com>
2023-07-28 16:08:52 +0900

    Update script.js

--- third/script.js
+++ third/script.js
@@ -11,6 +11,7 @@ const windowWidth = 400;
 const windowHeight = 640;
 const debuggingArea = 400;
 
+
 function setup() {
     console.log("appWidth : " + appWidth);
     console.log("appHeight : " + appHeight);
@@ -44,84 +45,68 @@ function setup() {
 
     connectButton = createButton("connect");
     connectButton.position(300, debuggingArea + 20);
+    connectButton.style('width', '75px');  // Set width of button
+    connectButton.style('height', '35px');  // Set height of button
     connectButton.mousePressed(connectSocket);
 
     sendQRButton = createButton("sendQR");
-    sendQRButton.position(300, debuggingArea + 50);
+    sendQRButton.position(300, debuggingArea + 190);
+    sendQRButton.style('width', '75px');  // Set width of button
+    sendQRButton.style('height', '35px');  // Set height of button
     sendQRButton.mousePressed(sendQRString);
+
 
     server = createInput("wss://remosharp-public-server14.glitch.me/");
     server.position(135, debuggingArea + 20);
     server.style("width", "150px");
 
-    oneForthURL = createA("./third/third.html", "Server 14");
+    oneForthURL = createA("../third/third.html", "Server 14");
     oneForthURL.position(30, debuggingArea - 10);
     oneForthURL.style("color", "white");
 
-    oneFifthURL = createA("./first/first.html", "Server 15");
+    oneFifthURL = createA("../first/first.html", "Server 15");
     oneFifthURL.position(160, debuggingArea - 10);
     oneFifthURL.style("color", "white");
 
-    oneSixthURL = createA("./second/second.html", "Server 16");
+    oneSixthURL = createA("../second/second.html", "Server 16");
     oneSixthURL.position(290, debuggingArea - 10);
     oneSixthURL.style("color", "white");
 }
 
 function draw() {
     clear();
-    background(0);
+    background(64, 70, 47);
+	image(capture, 0, 0, capture.width, capture.height);
 
-    let resizedCap = capture.get(0,0, appWidth, debuggingArea);
-
-    image(resizedCap, 0, 0);
-
-    fill(0);
-    rect(0, debuggingArea - 20, appWidth, appHeight);
+    fill(64, 70, 47);
+    rect(0,debuggingArea - 20, windowWidth, windowHeight);
 
     fill(0);
     textSize(20);
-    text("Custom Server URL", 10, 35);
-    let code = getCodeFromCapture(capture, captureGraphic);
-    if (code) {
+    text("Server 14", 10, 35);
+	let code = getCodeFromCapture(capture, captureGraphic);
+	if (code) {
+		// text(code.data, 10, windowHeight - 10);
         QRString = code.data;
-    }
-    
-    fill(255); // 重新添加了这一行
-    // 检查 QRString 是否已定义
-    if (QRString) {
-        // 分割字符串
-        let QRTexts = QRString.split(";");
+		// text("QRCode : " + QRString.data, 10, debuggingArea + 10);
+        // if(socketCondition == 1){
+        //     socket.send(code.data);
+        // }
+	}
+    fill(255);
+	text("QRCode : ", 10, debuggingArea + 95);
+    textSize(40);
+    textAlign(CENTER);
+    text(QRString, windowWidth / 2, debuggingArea + 155);
+    textAlign(LEFT);
+    textSize(20);
+    text("Server URL : ", 10, debuggingArea + 35);
 
-        // 计算每行文本的起始高度
-        let startHeight = debuggingArea + 95;
+    checkSocket();
+    text(socketCondition, 10, debuggingArea + 65);
 
-        // 检查是否有足够的元素
-        if (QRTexts.length >= 3) {
-            // 遍历文本数组，逐行显示
-            text("       ID        : " + QRTexts[0], 10, startHeight + 0);  
-            text("Discription : " + QRTexts[1], 10, startHeight + 30);
-            // 创建一个超链接并设置其位置
-            text("ImageLink   : ", 10, startHeight + 90);
-            let link = createA(QRTexts[2], QRTexts[2], "_blank");
-            link.position(10, startHeight + 110);
-        } else {
-            text("QRCode seems have some error ", 10, startHeight + 0);
-        }
 
-        text("Server URL : ", 10, debuggingArea + 35);
-
-        checkSocket();
-        text(socketCondition, 10, debuggingArea + 65);
-    }
-    else{
-        text("Server URL : ", 10, debuggingArea + 35);
-        checkSocket();
-        text(socketCondition, 10, debuggingArea + 65);
-    }
 }
-
-
-
 
 function connectSocket(){
     socket = new WebSocket(server.value());
